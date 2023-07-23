8f50d5cb4d2bedf561f6e40c82eab79180c3f9fb
Numereek <contact.numereek@gmail.com>
2023-07-23 17:44:49 +0430

    Initialize

--- script/modal.js
+++ script/modal.js
@@ -2,17 +2,30 @@ const newSalesBtn = document.getElementById("newSalesBtn");
 const newProductBtn = document.getElementById("newProductBtn");
 const newClientBtn = document.getElementById("newClientBtn");
 
+const clientModalCancel = document.getElementById("clientModalCancel");
+const salesModalCancel = document.getElementById("salesModalCancel");
+const productModalCancel = document.getElementById("productModalCancel");
+
 newSalesBtn.addEventListener("click", function () {
   salesPagemodalMask.style.display = "block";
-  console.log("newSalesBtn cliqué");
 });
 
 newProductBtn.addEventListener("click", function () {
   productPagemodalMask.style.display = "block";
-  console.log("newSalesBtn cliqué");
 });
 
 newClientBtn.addEventListener("click", function () {
   clientPagemodalMask.style.display = "block";
-  console.log("newSalesBtn cliqué");
+});
+
+clientModalCancel.addEventListener("click", function () {
+  clientPagemodalMask.style.display = "none";
+});
+
+salesModalCancel.addEventListener("click", function () {
+  salesPagemodalMask.style.display = "none";
+});
+
+productModalCancel.addEventListener("click", function () {
+  productPagemodalMask.style.display = "none";
 });
