f8e710f6bd7e151b8901bb19ecfee75a85f550d0
Numereek <contact.numereek@gmail.com>
2023-07-23 11:29:06 +0430

    Initialize

--- script/script.js
+++ script/script.js
@@ -1,3 +1,9 @@
+const searchPage = document.getElementById("searchPage");
+const clientPage = document.getElementById("clientPage");
+const salesPage = document.getElementById("salesPage");
+const productPage = document.getElementById("productPage");
+
+/*--------------------Nav links variables--------------------*/
 const searchPageSearch = document.getElementById("searchPageSearch");
 const productPageSearch = document.getElementById("productPageSearch");
 const clientPageSearch = document.getElementById("clientPageSearch");
@@ -11,15 +17,43 @@ const salesPageSales = document.getElementById("salesPageSales");
 const searchPageProduct = document.getElementById("searchPageProduct");
 const productPageProduct = document.getElementById("productPageProduct");
 const clientPageProduct = document.getElementById("clientPageProduct");
-const searchPageSearch = document.getElementById("searchPageSearch");
+const salesPageProduct = document.getElementById("salesPageProduct");
 
-const searchPageSales = document.getElementById("searchPageSales");
-const productPageSales = document.getElementById("productPageSales");
-const searchPageSearch = document.getElementById("searchPageSearch");
-const searchPageSearch = document.getElementById("searchPageSearch");
+const searchPageClient = document.getElementById("searchPageClient");
+const productPageClient = document.getElementById("productPageClient");
+const clientPageClient = document.getElementById("clientPageClient");
+const salesPageClient = document.getElementById("salesPageClient");
+
+/*--------------------Search Page Nav--------------------*/
 
 searchPageSearch.addEventListener("click", function () {
+  searchPage.style.display = "none";
+  clientPage.style.display = "none";
+  productPage.style.display = "none";
+  salesPage.style.display = "grid";
   console.log("searchPageSearch cliqué");
+});
+
+searchPageSales.addEventListener("click", function () {
+  searchPage.style.display = "none";
+  clientPage.style.display = "none";
+  productPage.style.display = "none";
+  salesPage.style.display = "grid";
+  console.log("searchPageSales cliqué");
+});
+searchPageProduct.addEventListener("click", function () {
+  searchPage.style.display = "none";
+  clientPage.style.display = "none";
+  productPage.style.display = "grid";
+  salesPage.style.display = "none";
+  console.log("searchPageProduct cliqué");
+});
+searchPageClient.addEventListener("click", function () {
+  searchPage.style.display = "none";
+  clientPage.style.display = "grid";
+  productPage.style.display = "none";
+  salesPage.style.display = "none";
+  console.log("searchPageClient cliqué");
 });
 
 function navigatePage() {}
