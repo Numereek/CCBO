535f8fbb62dc907e63933d22b22ae420a4693af6
Numereek <contact.numereek@gmail.com>
2023-07-23 11:08:13 +0430

    Initialize

---
+++ script/script.js
@@ -0,0 +1,25 @@
+const searchPageSearch = document.getElementById("searchPageSearch");
+const productPageSearch = document.getElementById("productPageSearch");
+const clientPageSearch = document.getElementById("clientPageSearch");
+const salesPageSearch = document.getElementById("salesPageSearch");
+
+const searchPageSales = document.getElementById("searchPageSales");
+const productPageSales = document.getElementById("productPageSales");
+const clientPageSales = document.getElementById("clientPageSales");
+const salesPageSales = document.getElementById("salesPageSales");
+
+const searchPageProduct = document.getElementById("searchPageProduct");
+const productPageProduct = document.getElementById("productPageProduct");
+const clientPageProduct = document.getElementById("clientPageProduct");
+const searchPageSearch = document.getElementById("searchPageSearch");
+
+const searchPageSales = document.getElementById("searchPageSales");
+const productPageSales = document.getElementById("productPageSales");
+const searchPageSearch = document.getElementById("searchPageSearch");
+const searchPageSearch = document.getElementById("searchPageSearch");
+
+searchPageSearch.addEventListener("click", function () {
+  console.log("searchPageSearch cliqué");
+});
+
+function navigatePage() {}
