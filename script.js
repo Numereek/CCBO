c20755d9b087fa62b61013331db0bd61239a9a5c
Numereek <contact.numereek@gmail.com>
2023-07-25 10:55:27 +0430

    Initialize

---
+++ script.js
@@ -0,0 +1,12 @@
+const searchPage = document.getElementById("searchPage");
+const clientPage = document.getElementById("clientPage");
+const productPage = document.getElementById("productPage");
+const salesPage = document.getElementById("salesPage");
+
+const 
+
+function navigate(activatePage) {
+  activatePage.style.display = "block";
+  
+}
+
