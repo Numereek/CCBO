10e2ffb3bd23bd9483ed7319d915fc3daf5f245f
Numereek <contact.numereek@gmail.com>
2023-07-25 11:52:47 +0430

    Initialize

--- script.js
+++ script.js
@@ -3,10 +3,32 @@ const clientPage = document.getElementById("clientPage");
 const productPage = document.getElementById("productPage");
 const salesPage = document.getElementById("salesPage");
 
-const 
+const navSearch = document.getElementById("navSearch");
+const navSales = document.getElementById("navSales");
+const navProduct = document.getElementById("navProduct");
+const navClient = document.getElementById("navClient");
 
-function navigate(activatePage) {
-  activatePage.style.display = "block";
-  
-}
-
+navSearch.addEventListener("click", () => {
+  searchPage.style.display = "block";
+  clientPage.style.display = "none";
+  productPage.style.display = "none";
+  salesPage.style.display = "none";
+});
+navSales.addEventListener("click", () => {
+  searchPage.style.display = "none";
+  clientPage.style.display = "none";
+  productPage.style.display = "none";
+  salesPage.style.display = "block";
+});
+navProduct.addEventListener("click", () => {
+  searchPage.style.display = "none";
+  clientPage.style.display = "none";
+  productPage.style.display = "block";
+  salesPage.style.display = "none";
+});
+navClient.addEventListener("click", () => {
+  searchPage.style.display = "none";
+  clientPage.style.display = "block";
+  productPage.style.display = "none";
+  salesPage.style.display = "none";
+});
