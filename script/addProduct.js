b93202c34ef83a6ac73f4458669d36eeade91e01
Numereek <contact.numereek@gmail.com>
2023-07-25 21:50:16 +0430

    Initialize

--- script/addProduct.js
+++ script/addProduct.js
@@ -13,11 +13,18 @@ function addProductInput() {
     <input type="text" name="salesModalProduct" class="salesModalProduct" id="salesModalProduct" placeholder="Product" style="width: 120px">
     <input type="text" name="salesModalQty" class="salesModalQty" id="salesModalQty" placeholder="Quantity" style="width: 20px">
     </div>`;
-  clickCount++;
-  insertInputPosition.insertAdjacentHTML("afterend", inputHMTL);
-  resizeContainer();
+  if (clickCount < 9) {
+    clickCount++;
+    insertInputPosition.insertAdjacentHTML("afterend", inputHMTL);
+    resizeContainer();
+  } else {
+    addAnotherProduct.innerText = "⚠ max input number";
+    addAnotherProduct.style.color = "red";
+    addAnotherProduct.style.textDecoration = "none";
+  }
 }
 
 addAnotherProduct.addEventListener("click", function () {
   addProductInput();
+  console.log(clickCount);
 });
